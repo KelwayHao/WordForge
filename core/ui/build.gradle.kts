fa36c34a92d8beb52bd1c77b0218257728299760
de.mirgorod <de.mirgorod@gmail.com>
2025-02-04 22:36:05 +0100

    update 4

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     tasks.create("testClasses")
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
@@ -56,8 +56,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
